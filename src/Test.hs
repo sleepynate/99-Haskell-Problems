9a02de65b0bad5164a22c89a2096564e65858b09
Nathan Dotz <nathan.dotz@gmail.com>
2012-06-22 12:50:16 -0400

    Make it actually use the exit codes for cabal test

--- src/Test.hs
+++ src/Test.hs
@@ -4,7 +4,18 @@ import Test.HUnit
 import qualified Problems1_10
 import qualified Problems11_20
 
+import System.Exit
+
 tests = TestList (Problems1_10.tests1_10 ++ Problems11_20.tests11_20)
 
 main::IO()
-main = (runTestTT tests) >>= (\x -> putStrLn $ show x)
+main = do
+    results <- runTestTT tests
+    let errs = errors results
+        fails = failures results
+    exitWith (codeGet errs fails)
+
+codeGet errs fails
+ | fails > 0       = ExitFailure 2
+ | errs > 0        = ExitFailure 1
+ | otherwise       = ExitSuccess
