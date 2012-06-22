6ea6a53527345fe6eaab1abd83ccb2697cbce119
Nathan Dotz <nathan.dotz@gmail.com>
2012-06-22 11:46:41 -0400

    Move over to using cabal to test

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,4 @@
 module Main where
 
-import Test.HUnit
-import qualified Problems1_10
-import qualified Problems11_20
-
-tests = TestList (Problems1_10.tests1_10 ++ Problems11_20.tests11_20)
-
 main::IO()
-main = (runTestTT tests) >>= (\x -> putStrLn $ show x)
+main = putStrLn "These are my solutions to the 99 haskell problems!"
