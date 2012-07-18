37751b7f44014cfe10d0e1a960a2b0d2022989c0
Nathan Dotz <nathan.dotz@gmail.com>
2012-07-17 20:25:15 -0400

    Add problem 22

--- src/Problems21_30.hs
+++ src/Problems21_30.hs
@@ -18,4 +18,24 @@ problem21 = test ["Insert an element at a given position into a list."
                  ~: insertAt 'X' "abcd" 2
                  ~=? "aXbcd"]
 
-tests21_30 = [TestLabel "Problem 21" problem21]
+{-
+ - Problem 22
+ - Create a list containing all integers within a given range.
+ - Example:
+ - Prelude> range 4 9
+ - [4,5,6,7,8,9]
+ -}
+
+range :: Int -> Int -> [Int]
+range a b = if a == b
+            then [b]
+            else a : (range (a + 1) b)
+
+problem22 = test [ "Create a list containing all integers in a given range."
+                 ~: range 4 9
+                 ~=? [4,5,6,7,8,9]]
+
+
+
+tests21_30 = [TestLabel "Problem 21" problem21,
+              TestLabel "Problem 22" problem22]
